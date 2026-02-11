556e38db4cc5f6e43c2785617706a06fcd703b8c
techteachdva <pcarroll@davincicharterschool.org>
2026-02-11 01:04:51 -0600

    Update Dungeon Class Export

--- src/site/dungeonclass/Dungeon_Class.pck
+++ src/site/dungeonclass/Dungeon_Class.pck
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45ae1b4fed33d9f5d4fe001f489112d29cc2bbde67a0776611b47593dd7e4e99
+oid sha256:ec15286cfabaeffd8ceabef3e17c3a093091ab519a95e2587123dbec65f64ebb
 size 66826976
--- src/site/dungeonclass/Dungeon_Class.service.worker.js
+++ src/site/dungeonclass/Dungeon_Class.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770792258|3950368982';
+const CACHE_VERSION = '1770793194|136770345';
 /** @type {string} */
 const CACHE_PREFIX = 'Dungeon Class-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
