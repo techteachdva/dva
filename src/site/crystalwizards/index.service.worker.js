d10bd7502bc58aa973b01c1790a941fd820ce142
techteachdva <pcarroll@davincicharterschool.org>
2026-02-16 21:46:15 -0600

    fuxin the start game screen change

--- src/site/crystalwizards/index.service.worker.js
+++ src/site/crystalwizards/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771297104|4410058715';
+const CACHE_VERSION = '1771299907|7213289039';
 /** @type {string} */
 const CACHE_PREFIX = 'Crystal Wizards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
