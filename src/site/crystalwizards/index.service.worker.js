0c3d3bcb7596351a2bac995a93799d022426d486
techteachdva <pcarroll@davincicharterschool.org>
2026-02-17 16:46:36 -0600

    Newest Sound Fix?

--- src/site/crystalwizards/index.service.worker.js
+++ src/site/crystalwizards/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771363726|1756496246';
+const CACHE_VERSION = '1771368190|6220199121';
 /** @type {string} */
 const CACHE_PREFIX = 'Crystal Wizards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
