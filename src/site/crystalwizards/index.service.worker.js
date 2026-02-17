d1f7972a5f3c317fe7ebf65227df1424e4c69bb4
techteachdva <pcarroll@davincicharterschool.org>
2026-02-16 20:49:52 -0600

    Mo Features Mo Bugs

--- src/site/crystalwizards/index.service.worker.js
+++ src/site/crystalwizards/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771294596|1901879919';
+const CACHE_VERSION = '1771296529|3835150944';
 /** @type {string} */
 const CACHE_PREFIX = 'Crystal Wizards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
