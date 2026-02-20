b25dd0765fb53d91b8ed8baed7d086a812b242df
techteachdva <pcarroll@davincicharterschool.org>
2026-02-20 14:19:11 -0600

    Online Player Turn Transition Bug

--- src/site/crystalwizards/index.service.worker.js
+++ src/site/crystalwizards/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771561159|6940379665';
+const CACHE_VERSION = '1771618614|1632834315';
 /** @type {string} */
 const CACHE_PREFIX = 'Crystal Wizards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
