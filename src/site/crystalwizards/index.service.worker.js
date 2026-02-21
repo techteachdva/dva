eae5e086486b8647bfa6481a1bbc3ff631febb6d
techteachdva <pcarroll@davincicharterschool.org>
2026-02-20 22:48:13 -0600

    Safari Tutorial Fallbacks

--- src/site/crystalwizards/index.service.worker.js
+++ src/site/crystalwizards/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771647361|567238769';
+const CACHE_VERSION = '1771649165|2371531247';
 /** @type {string} */
 const CACHE_PREFIX = 'Crystal Wizards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
