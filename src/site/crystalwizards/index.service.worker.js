fc1ddaac9cab3a4f0f5fecebbe357843513a8d73
techteachdva <pcarroll@davincicharterschool.org>
2026-02-21 11:45:13 -0600

    Mobile Update

--- src/site/crystalwizards/index.service.worker.js
+++ src/site/crystalwizards/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771694111|39417107713';
+const CACHE_VERSION = '1771695695|41000847871';
 /** @type {string} */
 const CACHE_PREFIX = 'Crystal Wizards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
