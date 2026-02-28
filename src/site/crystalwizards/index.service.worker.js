798dcc379b4bc4c9e659455093ca6d791444ae00
techteachdva <pcarroll@davincicharterschool.org>
2026-02-28 09:57:23 -0600

    Added Blocking to Tutorial

--- src/site/crystalwizards/index.service.worker.js
+++ src/site/crystalwizards/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772253683|1844250407';
+const CACHE_VERSION = '1772294030|4675332886';
 /** @type {string} */
 const CACHE_PREFIX = 'Crystal Wizards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
