daa98b1a9dd3e3e93baac1d1ccb612c986298a8a
techteachdva <pcarroll@davincicharterschool.org>
2026-03-01 21:13:42 -0600

    Crystals Graphically Represented, Pawns instead of Standees, Highlights Solidified, Spaces Adjusted, GUI Overhaul

--- src/site/crystalwizards/index.service.worker.js
+++ src/site/crystalwizards/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772294030|4675332886';
+const CACHE_VERSION = '1772420843|435805027';
 /** @type {string} */
 const CACHE_PREFIX = 'Crystal Wizards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
