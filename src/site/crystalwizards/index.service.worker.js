44fc0e7e1f0082a441b86f0f4092c8ce3309e192
techteachdva <pcarroll@davincicharterschool.org>
2026-03-11 07:57:52 -0500

    Skeletons Added to Dungeon Class

--- src/site/crystalwizards/index.service.worker.js
+++ src/site/crystalwizards/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772749781|1091976723';
+const CACHE_VERSION = '1772830727|66586572';
 /** @type {string} */
 const CACHE_PREFIX = 'Crystal Wizards-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
