def497df3f5a4231c0fbab9bbb6ce8afdabae3e5
DMZemo <161098275+DMZemo@users.noreply.github.com>
2026-05-18 18:33:12 -0500

    Retry Bug Fix

--- src/site/physix/physix.service.worker.js
+++ src/site/physix/physix.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779143871|89699531';
+const CACHE_VERSION = '1779146782|3000552965';
 /** @type {string} */
 const CACHE_PREFIX = 'Physix-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
