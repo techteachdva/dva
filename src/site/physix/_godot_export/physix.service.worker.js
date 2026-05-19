4a8e7be0f170a67595d61dd75608ac117162d417
DMZemo <161098275+DMZemo@users.noreply.github.com>
2026-05-18 19:10:59 -0500

    Another Safari Try

--- src/site/physix/_godot_export/physix.service.worker.js
+++ src/site/physix/_godot_export/physix.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779147841|4059448854';
+const CACHE_VERSION = '1779149141|153021232';
 /** @type {string} */
 const CACHE_PREFIX = 'Physix-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
